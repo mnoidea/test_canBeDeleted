a9858e94c0610a2314c235d841a4bab19d05a463
Morten Rasmussen <mortenrasmussen78@hotmail.com>
2013-03-13 14:13:52 +0100

    BowlingGame Startup with UnitTests

---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public void Roll(int p)
+        {
+            
+        }
+
+        public object Score()
+        {
+            return 0;
+        }
+    }
+}
---
+++ BowlingGame/TestBowlingGame/GameTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace TestBowlingGame
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void CreateGame()
+        {
+            Game game = new Game();
+        }
+
+        [TestMethod]
+        public void RollOnePinGame()
+        {
+            Game game = new Game();
+            game.Roll(1);
+        }
+
+        [TestMethod]
+        public void Game_RollAllZeros_ShouldReturnZero()
+        {
+            var game = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+            Assert.AreEqual(0, game.Score());
+        }
+    }
+}
